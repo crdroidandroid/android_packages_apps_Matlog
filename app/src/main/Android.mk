8427ae439fe51aab50aaa466817fceda5bed12f7
GtrCraft <nickvanbokhorst@hotmail.com>
2021-07-12 12:32:20 +0200

    Remove frameworks/base wildcard
    Fixes: Specifies both LOCAL_SDK_VERSION (system_current) and
    LOCAL_PRIVATE_PLATFORM_APIS (true) but should specify only one.

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -39,11 +39,7 @@ LOCAL_PACKAGE_NAME := MatLog
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 
-ifneq (,$(wildcard frameworks/base))
-  LOCAL_SDK_VERSION := current
-else
-  LOCAL_SDK_VERSION := system_current
-endif
+LOCAL_SDK_VERSION := system_current
 
 include $(BUILD_PACKAGE)
 
